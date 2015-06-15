fe1f6b77768e0c5b96e139d47a74135595eaeb48
nhershey <nhershey7@gmail.com>
2015-06-15 23:40:20 +0200

    Added provider table

--- js/basic.js
+++ js/basic.js
@@ -15,9 +15,19 @@ jQuery(function ($) {
 
 	// Load dialog on click
 	$('.basic').click(function (e) {
-		$('#modal-content').modal();
+		if ($(this).hasClass('deleteProvider')) {
+			$('#modal-content-deleteProvider').modal();
+			$("#simplemodal-container").css("height","200px");
+			var id = $(this).attr('id');
+			var first = $("#tblProviderFirst"+id).html()
+			var last = $("#tblProviderLast"+id).html()
+			$("#deleteName").html(first + " " + last);
+		}
+		else {
+			$('#modal-content').modal();
+		}
 		if ($(this).attr('id') == "btnAddPatient") {
-			$("#simplemodal-container").css("height","360px");
+			$("#simplemodal-container").css("height","370px");
 		}
 		return false;
 	});
